d46ba9d4cf65d82e33ffdbdead39b840dc7728fe
Gustavo Cunha <gustavocunha@gmail.com>
2014-09-25 09:43:23 -0300

    Chalenge: Update to use control flow to calculate real dog years.

--- AgeOfLaika/ViewController.swift
+++ AgeOfLaika/ViewController.swift
@@ -38,8 +38,19 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
 
     @IBAction func calculateButton(sender: UIButton) {
-        let humanAge = Double((humanAgeTextField.text as NSString).doubleValue)
-        resultLabel.text = "You dog has \(humanAge * 7) years in Dog Years"
+        var humanAge = Double((humanAgeTextField.text as NSString).doubleValue)
+        var dogsAge = 0.0
+        // dog age is:
+        // human age * 10.5 on the first 2 years
+        // human age * 4 on the following ones
+        if humanAge > 2 {
+            dogsAge += 2 * 10.5
+            humanAge -= 2
+            dogsAge += humanAge * 4
+        }else {
+            dogsAge = humanAge * 10.5
+        }
+        resultLabel.text = "You dog has \(dogsAge) years in Dog Years"
         resultLabel.hidden = false
         self.view.endEditing(true)
     }
